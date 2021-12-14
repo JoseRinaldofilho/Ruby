4cbd1612dbd20025311c84fbb0297ee039e64488
Rinaldo <mundial.cel11@gmail.com>
2021-12-14 17:01:15 -0300

    aprendendo o pust  gets.chomp to_i to_s to_flo

--- basico_intermediario/var.rb
+++ basico_intermediario/var.rb
@@ -1,7 +1,10 @@
 #variavel
 #
-nome = 'jose'
-idade = 41
-cidade = 'sobradinho'
+puts "qual seu nome? "
+nome = gets.chomp.to_s
+puts "qual sua diade ? "
+idade = gets.chomp.to_i
+puts "qual sua cidade ? "
+cidade = gets.chomp.to_s
 
-puts "seja bem vindo ",nome, "idade", idade, " cidade ", cidade
+puts "seja bem vindo #{nome} idade #{idade} mora em #{cidade} "
